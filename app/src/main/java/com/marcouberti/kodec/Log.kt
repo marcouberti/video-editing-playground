42020b24f30c0403be628814123caab4f55f54ac
Marco Uberti <marcouberti84@gmail.com>
2020-12-02 09:35:35 +0100

    chor: method doc

--- app/src/main/java/com/marcouberti/kodec/Log.kt
+++ app/src/main/java/com/marcouberti/kodec/Log.kt
@@ -4,6 +4,8 @@ import android.util.Log
 
 /**
  * Logging utility method.
+ *
+ * Uses as TAG the library name and the class name for easy filtering.
  */
 fun Any.log(msg: String, tag: String = this::class.java.simpleName) {
     Log.d("Kodec - $tag", msg)
