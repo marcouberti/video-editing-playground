f377db97aebc97541b8bc5751ae766e39fef0c0b
Marco Uberti <marcouberti84@gmail.com>
2020-12-09 18:15:45 +0100

    feat: add user interaction

--- app/src/main/java/com/marcouberti/kodec/ui/gl/MyGLRenderer.kt
+++ app/src/main/java/com/marcouberti/kodec/ui/gl/MyGLRenderer.kt
@@ -21,6 +21,9 @@ class MyGLRenderer : GLSurfaceView.Renderer {
     private lateinit var mTriangle: Triangle
     private lateinit var mSquare: Square
 
+    @Volatile
+    var angle: Float = 0f
+
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
@@ -43,9 +46,6 @@ class MyGLRenderer : GLSurfaceView.Renderer {
         // Calculate the projection and view transformation
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
 
-        // Create a rotation transformation for the triangle
-        val time = SystemClock.uptimeMillis() % 4000L
-        val angle = 0.090f * time.toInt()
         Matrix.setRotateM(rotationMatrix, 0, angle, 0f, 0f, -1.0f)
 
         // Combine the rotation matrix with the projection and camera view
--- app/src/main/java/com/marcouberti/kodec/ui/gl/MyGLSurfaceView.kt
+++ app/src/main/java/com/marcouberti/kodec/ui/gl/MyGLSurfaceView.kt
@@ -2,10 +2,15 @@ package com.marcouberti.kodec.ui.gl
 
 import android.content.Context
 import android.opengl.GLSurfaceView
+import android.view.MotionEvent
 
 class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
 
     private val renderer: MyGLRenderer
+
+    private val TOUCH_SCALE_FACTOR: Float = 180.0f / 320f
+    private var previousX: Float = 0f
+    private var previousY: Float = 0f
 
     init {
 
@@ -19,6 +24,40 @@ class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
 
         // Render the view only when there is a change in the drawing data.
         // To allow the triangle to rotate automatically, this line is commented out:
-        //renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+        renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+    }
+
+    override fun onTouchEvent(e: MotionEvent): Boolean {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+
+        val x: Float = e.x
+        val y: Float = e.y
+
+        when (e.action) {
+            MotionEvent.ACTION_MOVE -> {
+
+                var dx: Float = x - previousX
+                var dy: Float = y - previousY
+
+                // reverse direction of rotation above the mid-line
+                if (y > height / 2) {
+                    dx *= -1
+                }
+
+                // reverse direction of rotation to left of the mid-line
+                if (x < width / 2) {
+                    dy *= -1
+                }
+
+                renderer.angle += (dx + dy) * TOUCH_SCALE_FACTOR
+                requestRender()
+            }
+        }
+
+        previousX = x
+        previousY = y
+        return true
     }
 }
