3137cff6c4bd67582adcfcd44782674204aabbaa
Marco Uberti <marcouberti84@gmail.com>
2020-12-09 17:38:26 +0100

    feat: add OpenGL ES shapes sample

--- app/src/main/java/com/marcouberti/kodec/ui/gl/MyGLRenderer.kt
+++ app/src/main/java/com/marcouberti/kodec/ui/gl/MyGLRenderer.kt
@@ -5,20 +5,65 @@ import javax.microedition.khronos.opengles.GL10
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
+import com.marcouberti.kodec.ui.gl.shapes.Square
+import com.marcouberti.kodec.ui.gl.shapes.Triangle
 
 class MyGLRenderer : GLSurfaceView.Renderer {
+
+    // vPMatrix is an abbreviation for "Model View Projection Matrix"
+    private val vPMatrix = FloatArray(16)
+    private val projectionMatrix = FloatArray(16)
+    private val viewMatrix = FloatArray(16)
+
+    private lateinit var mTriangle: Triangle
+    private lateinit var mSquare: Square
 
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+
+        // initialize a triangle
+        mTriangle = Triangle()
+        // initialize a square
+        mSquare = Square()
     }
 
     override fun onDrawFrame(unused: GL10) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+
+        mTriangle.draw(vPMatrix)
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
+
+        val ratio: Float = width.toFloat() / height.toFloat()
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
+    }
+}
+
+/**
+ * Utility method to compile a shader before using it.
+ */
+fun loadShader(type: Int, shaderCode: String): Int {
+
+    // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+    // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+    return GLES20.glCreateShader(type).also { shader ->
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode)
+        GLES20.glCompileShader(shader)
     }
 }
